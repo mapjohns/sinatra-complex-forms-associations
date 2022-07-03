4fff596db4276a44315f3926f7a2703a123a3b0c
Matthew <mattj20@gmail.com>
2022-07-02 17:25:25 -0700

    commit tree 145daae5f6a11f2d89b024b7621623c0c7f65998

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+Owner.create(name: "Carl")
+Owner.create(name: "Eddy")
+
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
